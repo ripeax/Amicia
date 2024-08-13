922a9a8343bd9d1dfb1ee160f4da116b79642445
JB <167708886+ripeax@users.noreply.github.com>
2024-08-13 11:42:36 -0400

    Update F_Map.h

---
+++ vector/F_Map.h
@@ -0,0 +1,10 @@
+/*
+HANDLE CreateFileMappingA(
+  [in]           HANDLE                hFile,
+  [in, optional] LPSECURITY_ATTRIBUTES lpFileMappingAttributes,
+  [in]           DWORD                 flProtect,
+  [in]           DWORD                 dwMaximumSizeHigh,
+  [in]           DWORD                 dwMaximumSizeLow,
+  [in, optional] LPCSTR                lpName
+);
+*/
