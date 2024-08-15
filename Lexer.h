0c8b9502ab6ebec82eaf6ad2f2e689f2b61cdbf2
Jacob <167708886+ripeax@users.noreply.github.com>
2024-08-15 04:47:24 +0100

    Update

--- Lexer.h
+++ Lexer.h
@@ -1,9 +1,5 @@
 #include <iostream>
 
-#include <vector>
-#include <unordered_map>
-#include <tuple>
-
 #include <cstdint>
 
 #include <string>
@@ -14,6 +10,8 @@
 
 
 #define DEBUG true
+#define BUF_MX_SZ 32
+#define BUF_MN_SZ 8
 
 class Lexer :
     protected SyntaxTokens
@@ -62,7 +60,93 @@ protected:
     std::tuple<short, std::string> tkn_key(int ptr_key);
 };
 
-class SynMap {
-    SynMap() {}
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+#include <array>
+namespace std{
+    class SynMap :
+    private Token
+{  
+    SynMap() {
+        
+    
+    }
     ~SynMap() {}
+
+private:
+    unordered_map<string, string> tknz;
+    void print_k_v(/*  ...  */ ) {
+        //helper fx
+        auto print_kv_tkn = [](const auto& key, const auto& value)
+            {
+                std::cout << "Key:[" << key << "] Value:[" << value << "]\n";
+            };
+        //C++17 itel & print
+        for (const auto & [key, value] : tknz)
+            print_kv_tkn(key, value);
+        //using only 'auto'
+        for (const auto& n : tknz)
+            print_kv_tkn(n.first, n.second);
+    }
+protected:
+    struct tkn_str {
+        string_view tknStr;
+        Tokenz obj_tkn{ Token::undef };
+
+        [[ nodiscard ]]
+        constexpr bool operator<(tkn_str const& rhs) const
+        {
+            return tknStr < rhs.tknStr;
+        }
+        template< typename T, size_t N >
+        [[ nodiscard ]]
+        constexpr array< T, N > sort(array< T, N > arr) noexcept
+        {
+            sort(begin(arr), end(arr));
+            return arr;
+        }
+
+        constexpr array keywords
+        {
+            sort
+            (
+                array
+                {
+                    tknStr{ "False" , obj_tkn::KwFalse },
+                    tknStr{ "None"  , obj_tkn::KwNone  },
+                    tknStr{ "True"  , obj_tkn::KwTrue  },
+
+                    tknStr{ "with"  , obj_tkn::KwWith  },
+                    tknStr{ "yield" , obj_tkn::KwYield }
+                }
+            )
+        };
+
+        constexpr auto operators
+        {
+            sort
+            (
+                array
+                {
+                    tknStr{ "!"  , obj_tkn::OpLogicalNot },
+                    tknStr{ "!=" , obj_tkn::OpNotEqual   },
+                    tknStr{ "%"  , obj_tkn::OpMod        },
+                    tknStr{ "%=" , obj_tkn::OpAssignMod  },
+
+                    tknStr{ "&"  , obj_tkn::OpBitwiseAnd      },
+                    tknStr{ "|"  , obj_tkn::OpBitwiseOr       },
+                    tknStr{ "|=" , obj_tkn::OpAssignBitwiseOr },
+                    tknStr{ "||" , obj_tkn::OpLogicalOr       },
+                    tknStr{ "~"  , obj_tkn::OpBitwiseNot      }
+                }
+            )
+        };
+    };
+};
+}
+
+enum class Token : uint8_t
+{
+    undef
 };
